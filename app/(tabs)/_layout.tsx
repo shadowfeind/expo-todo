5b7d5097f800214ef454cc93e28005fce24f9448
shadowfeind <travestroy007@gmail.com>
2025-02-17 11:05:53 +0545

    batman

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,21 +30,6 @@ export default function TabLayout() {
           ),
         }}
       />
-      <Tabs.Screen
-        name="about"
-        options={{
-          title: "About",
-          tabBarIcon: ({ color, focused }) => (
-            <Ionicons
-              name={
-                focused ? "information-circle" : "information-circle-outline"
-              }
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
     </Tabs>
   );
 }
